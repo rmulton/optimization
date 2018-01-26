a32a45a682fb975ba5b3349fdd6a46a068bfa2fc
rmulton <rmulton@gmail.com>
2018-01-26 14:46:25 +0100

    changed name to allow doing several modelisations

---
+++ crossword_solving_constraints_on_words.py
@@ -0,0 +1,47 @@
+from constraint_programming import constraint_programming
+from crossword_parsing import Crossword, read_words
+
+WORDS = "../words3.txt"
+CROSSWORD = "../crossword1.txt"
+
+words = read_words(WORDS)
+cw = Crossword(CROSSWORD)
+
+print("Creating var")
+var = {str(line): set([word for word in words[line.length()]]) for line in cw.hlines+cw.vlines}
+P = constraint_programming(var)
+
+lines = cw.hlines + cw.vlines
+
+# A word is used only once
+
+for length in words.keys():
+    print("Creating NEQ for length {}".format(length))
+    NEQ = {(word, other_word) for word in words[length] for other_word in words[length] if (word!=other_word)}
+    lines_of_length = [line for line in lines if line.length()==length]
+    for line in lines_of_length:
+        for other_line in lines_of_length:
+            if line!=other_line:
+                print("Adding neq for lines {} and {}".format(line, other_line))
+                P.addConstraint(str(line), str(other_line), NEQ)
+    del(NEQ)
+
+
+def get_intersection_possibilities(i, j, hlength, vlength):
+    return {(word, other_word) for word in words[hlength] for other_word in words[vlength] if (word[i]==other_word[j] and word!=other_word)}
+
+i = 0
+for intersection in cw.intersections:
+    i += 1
+    print("Creating intersection {}".format(i))
+    which_h = intersection['which'][0]
+    hline = cw.hlines[which_h]
+    which_v = intersection['which'][1]
+    vline = cw.vlines[which_v]
+    SAME_LETTER_INTERSECT = get_intersection_possibilities(*intersection['position_in_lines'], hline.length(), vline.length())
+    P.addConstraint(str(hline), str(vline), SAME_LETTER_INTERSECT)
+
+
+print("Solving")
+sol = P.solve()
+print(sol)
